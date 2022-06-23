93e954ac7b9b500107bd4f7333d3ce88da1000d7
Reem Ahmed <reem.mda0909@gmail.com>
2022-06-23 11:11:09 -0600

    add bulk to indexedDB fetch api to fetch transactions when offline

--- public/js/idb.js
+++ public/js/idb.js
@@ -50,7 +50,7 @@ function uploadTransaction() {
 getAll.onsuccess = function() {
     // if there was data in indexedDb's store, let's send it to the api server
     if (getAll.result.length > 0) {
-      fetch('/api/transaction', {
+      fetch('/api/transaction/bulk', {
         method: 'POST',
         body: JSON.stringify(getAll.result),
         headers: {
